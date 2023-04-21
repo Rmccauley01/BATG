b669d5a75c3a8b9ce26e5263233dce5b164297ce
RhinoMcCauley01 <rmccauley01@tamu.edu>
2023-04-20 19:28:57 -0500

    Attempting to fix github actions.

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -65,25 +65,28 @@ export default {
     },
 
     retrieveProductData() {
-      axios({
-        method: 'get',
-        url: 'https://docs.google.com/spreadsheets/d/1NFbXtCu2DqdEMWCk70BbInQkY4TAIq4d3p3W-JQJ5No/edit#gid=0',
-      }) .then(function (response) {
-        this.dataString = response.data.split("Sheet1")[1]
-        this.dataString = this.dataString.substring(2,this.dataString.indexOf('>'))
+      //axios({
+      //  method: 'get',
+      //  url: 'https://docs.google.com/spreadsheets/d/1NFbXtCu2DqdEMWCk70BbInQkY4TAIq4d3p3W-JQJ5No/edit#gid=0',
+      //}) .then(function (response) {
+      //  this.dataString = response.data.split("Sheet1")[1]
+      //  this.dataString = this.dataString.substring(2,this.dataString.indexOf('>'))
 
-        this.dataList = this.dataString.split("\n")
-        this.dataList.shift()
+      //  this.dataList = this.dataString.split("\n")
+      //  this.dataList.shift()
 
-        // TODO: In the current state, there is an " at the end of the last list item (on the end of the price)
-        let i = 0
-        let temp = []
-        while (i < this.dataList.length) {
-          temp = this.dataList[i].split(',')
-          this.dataList[i] = ({"name": temp[0], "id": temp[1], "price": temp[2], "sizes": ["Small", "Medium", "Large"]})
-          i += 1
-        }
-      }.bind(this))
+      //  // TODO: In the current state, there is an " at the end of the last list item (on the end of the price)
+      //  let i = 0
+      //  let temp = []
+      //  while (i < this.dataList.length) {
+      //    temp = this.dataList[i].split(',')
+      //    this.dataList[i] = ({"name": temp[0], "id": temp[1], "price": temp[2], "sizes": ["Small", "Medium", "Large"]})
+      //    i += 1
+      //  }
+      //}.bind(this))
+      //
+
+      console.log("Test test, 1, 2")
     },
 
     toggleDrawer() {

4094c7885b549e4b4c0adfaa4be87f3a90e68b66
RhinoMcCauley01 <rmccauley01@tamu.edu>
2023-06-02 19:31:31 -0500

    Got a transition working, image not centered.

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app style="background-color:black">
     <NavigationBar 
       :drawer="drawer"
       :items="items"
--- src/components/SlideShow.vue
+++ src/components/SlideShow.vue
@@ -1,36 +1,80 @@
 <template>
-  <v-container
-    fluid
+  <v-container 
+    class="slideshow-wrapper mt-3" 
+    fluid 
     height="auto"
-    align="center"
     style="background-color:black"
   >
-    <v-img
-      :src="require('@/assets/slideshow_images/weights1.jpg')"
-      max-width="1024"
-      max-height="576"
-    >
-    </v-img>
+    <transition name="crossfade">
+      <v-img
+        :key="currentSlideIndex"
+        :src="currentSlide.src"
+        max-width="1024"
+        max-height="576"
+        class="crossfade-image"
+      ></v-img>
+    </transition>
   </v-container>
 </template>
 
+<style>
+.slideshow-wrapper {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 40%;
+}
+
+.crossfade-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.crossfade-enter-active,
+.crossfade-leave-active {
+  transition: opacity 1s;
+}
+
+.crossfade-enter,
+.crossfade-leave-to {
+  opacity: 0;
+}
+</style>
+
 <script>
-
-  export default {
-
-    name: "SlideShow",
-
-    data() {
-      return {
-        slides: [
-          {
-            src: require('@/assets/slideshow_images/weights1.jpg')
-          },
-          {
-            src: require('@/assets/slideshow_images/manLifting1.jpg')
-          },
-        ],
-      }
+export default {
+  name: "SlideShow",
+  data() {
+    return {
+      slides: [
+        {
+          src: require("@/assets/slideshow_images/weights1.jpg"),
+        },
+        {
+          src: require("@/assets/slideshow_images/manLifting1.jpg"),
+        },
+      ],
+      currentSlideIndex: 0,
+    };
+  },
+  computed: {
+    currentSlide() {
+      return this.slides[this.currentSlideIndex];
     },
-  }
+  },
+  mounted() {
+    this.startSlideshow();
+  },
+  methods: {
+    startSlideshow() {
+      setInterval(() => {
+        this.currentSlideIndex =
+          (this.currentSlideIndex + 1) % this.slides.length;
+      }, 5000);
+    },
+  },
+};
 </script>

cac92730b0525516db5c7e1821f6236b754c4b71
Ryan McCauley <62566983+Rmccauley01@users.noreply.github.com>
2023-06-03 10:47:15 -0500

    Implemented custom carousel to fix some issues. (#10)
    * Implemented image placeholder and fixed navbar issue.
    
    * Got a transition working, image not centered.
    
    * Centered slideshow, functionality complete.
    
    ---------
    
    Co-authored-by: RhinoMcCauley01 <rmccauley01@tamu.edu>

--- src/components/SlideShow.vue
+++ src/components/SlideShow.vue
@@ -1,71 +1,81 @@
 <template>
-  <v-carousel
-    id="scrolling-techniques-4"
-    cycle
-    :interval="8000"
-    hide-delimiter-background
-    :show-arrows="false"
+  <v-container 
+    class="slideshow-wrapper" 
+    fluid 
     height="auto"
+    style="background-color:black"
   >
-    <v-sheet 
-      color="black" 
-      height="100%" 
-      width="100%" 
-      align="center"
-    >
-      <v-carousel-item
-        v-for="(slide, i) in slides"
-        :key="i"
-        :src="slide.src"
+    <transition name="crossfade">
+      <v-img
+        :key="currentSlideIndex"
+        :src="currentSlide.src"
         max-width="1024"
         max-height="576"
-        fade
-      >
-      </v-carousel-item>
-    </v-sheet>
-  </v-carousel>
+        class="crossfade-image"
+      ></v-img>
+    </transition>
+  </v-container>
 </template>
 
+<style>
+.slideshow-wrapper {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 40%;
+}
+
+.crossfade-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin:auto;
+}
+
+.crossfade-enter-active,
+.crossfade-leave-active {
+  transition: opacity 5s;
+}
+
+.crossfade-enter,
+.crossfade-leave-to {
+  opacity: 0;
+}
+</style>
+
 <script>
-  export default {
-
-    name: "SlideShow",
-
-    data() {
-      return {
-        slideHeight: window.innerWidth - 200,
-        slides: [
-          {
-            src: require('@/assets/slideshow_images/weights1.jpg')
-          },
-          {
-            src: require('@/assets/slideshow_images/manLifting1.jpg')
-          },
-        ],
-      }
+export default {
+  name: "SlideShow",
+  data() {
+    return {
+      slides: [
+        {
+          src: require("@/assets/slideshow_images/weights1.jpg"),
+        },
+        {
+          src: require("@/assets/slideshow_images/manLifting1.jpg"),
+        },
+      ],
+      currentSlideIndex: 0,
+    };
+  },
+  computed: {
+    currentSlide() {
+      return this.slides[this.currentSlideIndex];
     },
-
-    computed: {
-      maxCarouselHeight() {
-        const sheetHeight = this.$refs.sheet.clientHeight;
-        const itemHeight = this.$refs.item.clientHeight;
-        return `${(itemHeight / sheetHeight) * 100}%`;
-      }
+  },
+  mounted() {
+    this.startSlideshow();
+  },
+  methods: {
+    startSlideshow() {
+      setInterval(() => {
+        this.currentSlideIndex =
+          (this.currentSlideIndex + 1) % this.slides.length;
+      }, 5000);
     },
-
-    mounted() {
-      window.addEventListener("resize", this.handleResize);
-    },
-
-    unmounted() {
-      window.removeEventListener("resize", this.handleResize);
-    },
-
-    methods: {
-      handleResize() {
-        this.screenWidth = window.innerWidth;
-        this.slideHeight = window.innerWidth - 200;
-      }
-    }
-  }
+  },
+};
 </script>
